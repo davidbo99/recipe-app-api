34dbac43047e2aea9117d522bd9f30b181c91b46
David Bolivar <david-702bo@hotmail.com>
2022-11-13 21:45:57 -0500

    Add new features to django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -16,16 +16,31 @@ class UserAdmin(BaseUserAdmin):
         (
             _('Permissions'),
             {
-                'fields': {
+                'fields': (
                     'is_active',
                     'is_staff',
                     'is_superuser',
-                }
+                )
             }
         ),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
     readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': {'wide',},
+            'fields': {
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+
+            }
+        }),
+    )
 
 
 admin.site.register(models.User, UserAdmin)
